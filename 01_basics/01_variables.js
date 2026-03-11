f398ff896f1522a36e8b2166555bcda60f75f02f
NIKHIL KUMAR <nikhilkumar301713@gmail.com>
2026-03-11 18:23:05 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,10 @@
-const accountId = 144553
-let accountEmail = "hitesh@google.com"
-var accountPassword = "12345"
+//in js you can ignore ; at end of each line (for safety use ; always)
+const accountId = 144553;  //const-its value cannot be changed 
+//variables are assigned using "let" and "var"=>variables can be changed later but const cannot
+let accountEmail = "hitesh@google.com"; 
+var accountPassword = "12345";
 accountCity = "Jaipur"  //in js we can use variables without datatype
-let accountState;    //no val assigned-output=undefined
+let accountState;    //no val assigned=>output=undefined
 
 // accountId = 2 // not allowed (cannot change const values)
 
@@ -15,6 +17,8 @@ console.log(accountId);
 /*
 Prefer not to use var
 because of issue in block scope and functional scope
+
+    scope=>{}
 */
 
 
