7415af99189e9c94bfdb2e7f5f12cfe39a973212
NIKHIL KUMAR <nikhilkumar301713@gmail.com>
2026-03-11 18:33:07 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,30 +1,32 @@
 
 "use strict"; // treat all JS code as newer version
 
-// alert( 3 + 3) // we are using nodejs, not browser
+// alert( 3 + 3) // error because we are using nodejs, not browser
 
 console.log(3 
     +
      3) // code readability should be high
 
 console.log("Hitesh")
-
+//documentation of alert=>tc39.es  ecmascript
 
 let name = "hitesh"
 let age = 18
 let isLoggedIn = false
 let state;
 
-// number => 2 to power 53
-// bigint
+// number => range is 2 to power 53
+// bigint ->for large numbers
 // string => ""
 // boolean => true/false
-// null => standalone value
-// undefined => no value is assigned now
+// null => standalone value->representaiion for empty val(no val is recieved)
+// undefined => variable is declared but no value is assigned now
 // symbol => to find uniqueness
 
 
 // object
 
+console.log(typeof age); //number
+console.log(typeof "nikhil"); //string
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
