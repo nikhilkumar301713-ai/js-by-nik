f5a7fcbbac55c250127f607ea2bcdd425c11777b
NIKHIL KUMAR <nikhilkumar301713@gmail.com>
2026-03-20 11:19:45 +0000

    promises

--- 08_API/promises.js
+++ 08_API/promises.js
@@ -149,3 +149,33 @@ async function consumePromiseFour() {
 }
 
 consumePromiseFour();
+
+//FETCH
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+
+//getAllUsers()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+// promise.all
+
+
+
+/*the global fetch() method starts the process of fetching a resource
+from the network ,returning a promise which is fulfilled once the response is available*/
